f93ad917bf262e3f7738bd0caa166620aeb26326
ValientJustice <hcs033469@mail.helenacollege.edu>
2026-04-20 15:02:31 -0600

    modified:   src/main/java/edu/helenacollege/hctickets/dto/TicketCommentCreateDto.java 	modified:   src/main/java/edu/helenacollege/hctickets/repository/UserApplicationRoleRepository.java 	modified:   src/main/resources/application.properties

--- src/main/java/edu/helenacollege/hctickets/dto/TicketCommentCreateDto.java
+++ src/main/java/edu/helenacollege/hctickets/dto/TicketCommentCreateDto.java
@@ -6,4 +6,11 @@ public record TicketCommentCreateDto(
         String commentText,
         Boolean internalComment,
         Integer parentCommentId
-) {}
+) {
+	public TicketCommentCreateDto() {
+		this(0, 0, "", true, 0);
+	}
+	public TicketCommentCreateDto(Integer ticketId, Integer userId) {
+		this(ticketId, userId, "", true, 0);
+	}
+}
--- src/main/java/edu/helenacollege/hctickets/repository/UserApplicationRoleRepository.java
+++ src/main/java/edu/helenacollege/hctickets/repository/UserApplicationRoleRepository.java
@@ -1,7 +1,12 @@
 package edu.helenacollege.hctickets.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import edu.helenacollege.hctickets.model.User;
 import edu.helenacollege.hctickets.model.UserApplicationRole;
 public interface UserApplicationRoleRepository extends JpaRepository<UserApplicationRole, Integer> {
+	//List<UserApplicationRole> findUserApplicationRoleWithUser(@Param("userId") int userId);
 }
